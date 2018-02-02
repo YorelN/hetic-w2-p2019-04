c0c1b5b09cf83ec4de72f6c8fa3c9e405d5d7351
Nicolas LEROY <nicolas.leroy@hetic.net>
2018-02-02 01:50:42 +0100

    Perf ecran (#11)
    * Comming soon  + contact pages
    
    * burger menu

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,21 @@
-import { TweenMax, TimelineLite } from 'gsap'
 import '../scss/styles.scss';
+
+
+const navLogo = document.querySelector('.nav__logo');
+const burgerMenu = document.querySelector('.burgerMenu');
+const closeButtonMenu = document.querySelector('.closeButton');
+const burgerMEnuLinks = document.querySelectorAll('.burgerMenu li');
+navLogo.addEventListener('click', function () {
+    burgerMenu.classList.toggle('active');
+    // Don't work, will fix it later
+    console.log(burgerMEnuLinks);
+    burgerMEnuLinks.forEach((link, key) => {
+        // This shit don't work, help please ?
+        link.classList.add('showLi')
+    })
+});
+closeButtonMenu.addEventListener('click', function () {
+    burgerMenu.classList.toggle('active')
+});
+
+
