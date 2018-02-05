a979478d65a60067e4bf3db345b4ef97c7029d62
Nico <nicolas.leroy@hetic.net>
2018-02-05 11:49:28 +0100

    Create navBar

--- src/js/index.js
+++ src/js/index.js
@@ -5,17 +5,19 @@ const navLogo = document.querySelector('.nav__logo');
 const burgerMenu = document.querySelector('.burgerMenu');
 const closeButtonMenu = document.querySelector('.closeButton');
 const burgerMEnuLinks = document.querySelectorAll('.burgerMenu li');
-navLogo.addEventListener('click', function () {
-    burgerMenu.classList.toggle('active');
-    // Don't work, will fix it later
-    console.log(burgerMEnuLinks);
-    burgerMEnuLinks.forEach((link, key) => {
-        // This shit don't work, help please ?
-        link.classList.add('showLi')
-    })
-});
-closeButtonMenu.addEventListener('click', function () {
-    burgerMenu.classList.toggle('active')
-});
+if (navLogo) {
+    navLogo.addEventListener('click', function () {
+        burgerMenu.classList.toggle('active');
+        // Don't work, will fix it later
+        console.log(burgerMEnuLinks);
+        burgerMEnuLinks.forEach((link, key) => {
+            // This shit don't work, help please ?
+            link.classList.add('showLi')
+        })
+    });
+    closeButtonMenu.addEventListener('click', function () {
+        burgerMenu.classList.toggle('active')
+    });
+}
 
 
