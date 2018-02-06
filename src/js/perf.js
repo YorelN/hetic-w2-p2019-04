229083a32d32ffe21db94c26ab57b565ac772231
Nicolas LEROY <nicolas.leroy@hetic.net>
2018-02-06 02:14:17 +0100

    dynamic perf and responsive (#14)

---
+++ src/js/perf.js
@@ -0,0 +1,18 @@
+export const perf = {
+    speed: {
+        one: "15 MPH",
+        two: "20 MPH"
+    },
+    autonomy: {
+        one: "45 min" ,
+        two: "90 min"
+    },
+    weight: {
+        one: "5 KG",
+        two: "6.3 KG"
+    },
+    angle: {
+        one: "15 %",
+        two: "20 %"
+    }
+};
