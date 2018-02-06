6539330c751fff77608cbdae5b192bac38f60d2e
Nico <nicolas.leroy@hetic.net>
2018-02-06 02:13:52 +0100

    dynamic perf and responsive

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,5 @@
 import '../scss/styles.scss';
-
+import { perf } from "./perf";
 
 const navLogo = document.querySelector('.nav__logo');
 const burgerMenu = document.querySelector('.burgerMenu');
@@ -31,3 +31,89 @@ contactBtn.addEventListener('click', function () {
 closeBtn.addEventListener('click', function () {
     contactForm.classList.add('hidden');
 });
+
+
+const firstSectionImg = document.querySelector('.firstSectionImg');
+
+
+document.addEventListener('scroll', function () {
+    if (isInViewport(firstSectionImg) && firstSectionImg.className !== 'firstSectionImg active') {
+        firstSectionImg.classList.add('active');
+        firstSectionImg.style.backgroundPosition = '100% 0';
+
+    }
+});
+
+
+
+
+
+// perf section
+
+
+const perfContainer = document.querySelectorAll('.perfContainer div');
+
+
+
+for (let i = 0; i < perfContainer.length; i++) {
+    const currPerf = perfContainer[i];
+    const keyName = currPerf.textContent.toLowerCase();
+
+    currPerf.addEventListener('click', function () {
+        const perfKeyName = Object.keys(perf);
+        const perfValue = Object.values(perf);
+
+        const perfOne = document.querySelector('.perfKeyOne');
+        const perfTwo = document.querySelector('.perfKeyTwo');
+        const mesurePerfOne = document.querySelector('.mesurePerfKeyOne');
+        const mesurePerfTwo = document.querySelector('.mesurePerfKeyTwo');
+
+        const perfWithOne = perfValue[i].one.split(' ')[0];
+        const perfWithTwo = perfValue[i].two.split(' ')[0];
+        const mesurePerfWithOne = perfValue[i].one.split(' ')[1];
+        const mesurePerfWithTwo = perfValue[i].two.split(' ')[1];
+        if (keyName === perfKeyName[i]) {
+            perfOne.innerHTML = perfWithOne;
+            perfTwo.innerHTML = perfWithTwo;
+
+            mesurePerfOne.innerHTML = mesurePerfWithOne;
+            mesurePerfTwo.innerHTML = mesurePerfWithTwo;
+        }
+    })
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function isInViewport(element) {
+    var rect = element.getBoundingClientRect();
+    var html = document.documentElement;
+    return (
+        rect.top >= 0 &&
+        rect.left >= 0 &&
+        rect.bottom <= (window.innerHeight || html.clientHeight) &&
+        rect.right <= (window.innerWidth || html.clientWidth)
+    );
+};
